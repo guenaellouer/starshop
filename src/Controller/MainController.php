6a5c1026cd541c11395acacd41ae0180fcb00d9e
guenaellouer <guenael.louer@gmail.com>
2024-05-11 11:48:26 +0200

    deploiement sur github

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        //return new Response('Hello Starshop !');
+        $starshipCount = 427;
+        return $this->render('main/homepage.html.twig',[
+            'numberOfStarship' => $starshipCount,
+        ]);
+
+    }
+
+
+}
